7a81b5443b5b88d4f9556c6d30da7b1e148cf836
LadAnu2004 <anjalilad2112@gmail.com>
2026-02-23 19:32:03 +0530

    Completed Practical 4A

--- py3b_1.py
+++ py3b_1.py
@@ -3,8 +3,18 @@ for i in range(1, rows + 1):
     for j in range(i):
         print("*", end=" ")
     print()
+#practical 4 A
 
+rows = 5
+num = 1
 
+for i in range(rows, 0, -1):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+        if num == 10:   # Reset after 9 
+            num = 1
+    print()
 
 
 
